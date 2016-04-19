3434c8e1bbc3a7630395d620fee714f93a9146c0
Peter John Manuel <peterjohn.manuel@gmail.com>
2016-04-19 19:39:23 +0200

    Player initial movement logic enabled.

--- js/app.js
+++ js/app.js
@@ -1,23 +1,27 @@
+'use strict';
+
 // Enemies our player must avoid
-var Enemy = function() {
+var Enemy = function () {
     // Variables applied to each of our instances go here,
     // we've provided one for you to get started
 
     // The image/sprite for our enemies, this uses
     // a helper we've provided to easily load images
     this.sprite = 'images/enemy-bug.png';
+    this.x;
+    this.y;
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt) {
+Enemy.prototype.update = function (dt) {
     // You should multiply any movement by the dt parameter
     // which will ensure the game runs at the same speed for
     // all computers.
 };
 
 // Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
+Enemy.prototype.render = function () {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
@@ -26,15 +30,51 @@ Enemy.prototype.render = function() {
 // a handleInput() method.
 
 
+var Player = function () {
+    this.sprite = 'images/char-boy.png';
+    this.x = 205;
+    this.y = 380;
+}
+
+Player.prototype.update = function (dt) {
+    this.x;
+    this.y;
+}
+
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+}
+
+Player.prototype.handleInput = function (key) {
+
+    console.log(key);
+
+    switch (key) {
+        case 'up': this.y = this.y - 83;
+            this.render();
+            break;
+        case 'down': this.y = this.y + 83;
+            this.render();
+            break;
+        case 'left': this.x = this.x - 101;
+            this.render();
+            break;
+        case 'right': this.x = this.x + 101;
+            this.render();
+            break;
+    }
+}
+
 // Now instantiate your objects.
 // Place all enemy objects in an array called allEnemies
 // Place the player object in a variable called player
 
+var player = new Player();
 
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
-document.addEventListener('keyup', function(e) {
+document.addEventListener('keyup', function (e) {
     var allowedKeys = {
         37: 'left',
         38: 'up',
